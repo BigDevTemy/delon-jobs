ef6ddfd915cd448eb1ed8211c75fed82fd064f2d
temiloluwao <temiloluwao@phillipsoutsourcing.net>
2024-07-12 15:06:57 +0100

    Commit 2

--- app/Jobs/SubscribeMailJob.php
+++ app/Jobs/SubscribeMailJob.php
@@ -9,20 +9,24 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailable;
+use App\Mail\SendMails;
+use Mail;
 
 class SubscribeMailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    protected $EmailData;
+
+    protected $subscribers;
+
     /**
      * Create a new job instance.
      *
+     * @param array $subscribers
      * @return void
      */
-    public function __construct($EmailData)
+    public function __construct($subscribers)
     {
-        //
-        $this->emailData = $EmailData;
+        $this->subscribers = $subscribers;
     }
 
     /**
@@ -32,10 +36,19 @@ class SubscribeMailJob implements ShouldQueue
      */
     public function handle()
     {
-        //
-        $sendEmail = new SubscribeEmail($this->emailData);
-        $sendEmail->send();
-      
+        $email = new SendMails();
+
+        foreach ($this->subscribers as $subscriber) {
+            try {
+                Mail::to($subscriber['email'])
+                ->subject($subscriber['title'])
+                ->from('sender@example.com', $subscriber['name'])
+                ->send($email);
+            } catch (\Exception $e) {
+                // Handle any exceptions that may occur during the email sending process
+                // You can log the error, notify the administrator, or handle it in any other way
+                logger()->error('Error sending email to ' . $subscriber['email'] . ': ' . $e->getMessage());
+            }
+        }
     }
 }
-
